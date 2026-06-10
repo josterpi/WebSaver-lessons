a15ae58dfa8da259516a3aa978320934cf100842
Jeremy Osterhouse <josterhouse@gmail.com>
2026-06-10 11:58:50 -0400

    Technical debt and FIXME
    Code is not written perfectly in one go. As I'm working on a project,
    I'm trying to get as quickly as possible to something that works and
    then refine it. As I'm writing, I'll write things that work right now,
    but I know will have problems in the future. I'll add a FIXME comment to
    remind myself to come back later and implement a more permanent
    solution.
    
    In this case, I noticed that the QR code generator creates files where
    different users could easily create QR codes with the same name. It
    works for now, but isn't going to work long term. What's the solution? I
    don't know. Maybe create a subdirectory for each user, or save the file
    with a random filename, like a hash (md5, sha1), UUID, or the primary
    key of the model. It's a tricky little design problem that you don't
    have time to solve when you're first getting something working, so you
    add a FIXME comment.
    
    I added another FIXME comment where I noticed that generate_qr_code()
    builds the path for the QR code with settings.MEDIA_ROOT first, but then
    returns an absolute path, which gets saved in the DB, which you need to
    remember while working on the view. If MEDIA_ROOT ever changes, this
    will break everything in the DB.
    
    Part of gaining experience is learning to identify where you're making
    an intentional compromise for the time being so that you can come back
    later and fix it. You gain that experience by getting bitten when you
    need to make changes later and find that your earlier decisions are not
    making it more difficult. We call this "technical debt." Sometimes the
    tech debt grows because we're inexperienced. Sometimes there's a
    deadline and we need to get something working right now.
    
    There's some overlap between tech debt and bugs. If a user tried to save
    a QR code with the same name as an existing QR code and overwrote
    someone else's URL, you could get bug report that would lead you to
    track down the source.

--- URL_Library/main/qr_code.py
+++ URL_Library/main/qr_code.py
@@ -16,9 +16,10 @@ def generate_qr_code(url, name, size=6, border_size=4, color1='black', color2='w
     qr.make(fit=True)
 
     img = qr.make_image(fill_color=color1, back_color=color2)
-    file_name = f'{name}.png'
+    file_name = f'{name}.png'  # FIXME filename collision if the same name is used twice
     full_path = os.path.join(qr_code_dir, file_name)
     img.save(full_path)
     
     # Return relative path (for database and URLs)
+    # FIXME This gets saved in the DB which will break if settings.MEDIA_ROOT ever changes 
     return f'/media/qr_codes/{file_name}'
